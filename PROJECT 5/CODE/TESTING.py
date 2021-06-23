ab9dd17587f61f54d45dff9d756982814e83e312
Negative-light <negalight414@gmail.com>
2021-06-22 21:36:03 -0400

    Completed up to Model Validation

---
+++ PROJECT 5/CODE/TESTING.py
@@ -0,0 +1,31 @@
+# %%
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import numpy as np
+
+rng = np.random.RandomState(42)
+x = 10 * rng.rand(50)
+y = 2 * x - 1 + rng.randn(50)
+plt.scatter(x, y)
+
+
+# %%
+model = LinearRegression(fit_intercept=True)
+model
+X = x[:, np.newaxis]
+X.shape
+
+model.fit(X, y)
+model.coef_
+model.intercept_
+
+
+xfit = np.linspace(-1, 11)
+Xfit = xfit[:, np.newaxis]
+yfit = model.predict(Xfit)
+
+plt.clf()
+plt.scatter(x, y)
+plt.plot(xfit, yfit)
+# %%
+plt.show()
